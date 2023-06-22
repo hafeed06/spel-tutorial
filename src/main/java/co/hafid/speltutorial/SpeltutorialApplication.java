14a76a06f8e27c75c5faa0cd2df012ed022d3792
Abdelhafid El Bekkaoui <abelhafid.elbekkaoui>
2023-06-22 15:37:52 +0200

    Upgrade to 3.0.7

--- src/main/java/co/hafid/speltutorial/SpeltutorialApplication.java
+++ src/main/java/co/hafid/speltutorial/SpeltutorialApplication.java
@@ -1,10 +1,12 @@
 package co.hafid.speltutorial;
 
+import co.hafid.speltutorial.properties.StudentProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-//@EnableConfigurationProperties(NewDealerNetworkProperties.class)
+//@EnableConfigurationProperties({StudentProperties.class})
 public class SpeltutorialApplication {
 
 	public static void main(String[] args) {
--- src/main/java/co/hafid/speltutorial/properties/StudentProperties.java
+++ src/main/java/co/hafid/speltutorial/properties/StudentProperties.java
@@ -2,6 +2,7 @@ package co.hafid.speltutorial.properties;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -13,14 +14,14 @@ import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties(prefix = "app.properties")
+@AllArgsConstructor
 @Getter
-@Setter
 public class StudentProperties {
 
     private List<Student> students;
 
     @Getter
-    @Setter
+    @AllArgsConstructor
     public static class Student {
         private String firstName;
         private String lastNAme;
