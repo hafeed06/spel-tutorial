6b498fa859f2a657b6f4dcafb58034ac10060868
Abdelhafid El Bekkaoui <abelhafid.elbekkaoui>
2023-11-16 18:29:59 +0100

    Testing branch protection rules

--- src/main/java/co/hafid/speltutorial/SpeltutorialApplication.java
+++ src/main/java/co/hafid/speltutorial/SpeltutorialApplication.java
@@ -10,12 +10,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 public class SpeltutorialApplication {
 
 	public static void main(String[] args) {
-		try {
 			SpringApplication.run(SpeltutorialApplication.class, args);
-		}
-		catch(Exception e) {
-			System.out.println(e.getMessage());
-		}
 	}
 
 }
